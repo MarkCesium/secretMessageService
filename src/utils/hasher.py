e1041c138d5a49d6a79c3c2368f8f3c8632a8304
MarkCesium <danikhrenkov29@gmail.com>
2024-10-27 00:05:37 +0300

    Fix hasher with time

--- src/utils/hasher.py
+++ src/utils/hasher.py
@@ -1,7 +1,8 @@
 import hashlib
+from time import time
 
 
 def get_message_hash(text: str, salt: str) -> str:
     return hashlib.sha256(
-        text.encode('UTF-8') + salt.encode('UTF-8')
+        text.encode("UTF-8") + salt.encode("UTF-8") + str(time()).encode("UTF-8"),
     ).hexdigest()
